45b6955b218c7e5453f95b961b8a17d1676db49d
godmeowth <102175901+godmeowth@users.noreply.github.com>
2024-03-04 00:04:27 +0100

    complete set up

---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+    return (
+        <div className="p-7">
+            <p>Hello Admin </p>
+            <Button>Click me</Button>
+        </div>
+
+    );
+}
